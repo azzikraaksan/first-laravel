fbc1a0e43114e55d3aa9c50b7dd5e2fe0f7ef930
azzikraaksan <azzikraaksan@gmail.com>
2025-07-17 11:06:34 +0700

    bismillahirrohmanirrohiimm part11

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 [ ! -f .env ] && cp .env.example .env
 
 # Generate app key kalau belum ada
-php artisan key:generate
+php artisan key:generate --force
 php artisan storage:link
 php artisan migrate --force
 
@@ -12,6 +12,8 @@ php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+
+chmod -R 775 storage bootstrap/cache
 
 # Start Laravel dengan PHP built-in server
 php artisan serve --host=0.0.0.0 --port=8000
